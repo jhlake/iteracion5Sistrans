ac846d95ed5bd0e3e90ba495d2e75d9e47c84588
PAULA <PAULA@157.253.224.228>
2017-05-23 20:10:59 -0500

    Funciones

--- src/rest/FuncionesServices.java
+++ src/rest/FuncionesServices.java
@@ -1,11 +1,15 @@
 package rest;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Properties;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,6 +18,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import dtm.FestivAndesDistributed;
 import tm.FestivAndesMaster;
 import vos.Funcion;
 import vos.ListaFunciones;
@@ -97,5 +102,57 @@ public class FuncionesServices{
 			return Response.status(500).entity(doErrorMessage(e)).build();
 		}
 		return Response.status(200).entity(lista).build();
+	}
+	
+	/**
+	 * Método que expone servicio REST usando POST para inicializar los servicios de JMS
+	 * <b>URL: </b> http://"ip o nombre de host":8080/VideoAndes/rest/admin/jmsInit
+	 */
+	@POST
+	@Path("/jmsInit")
+	public Response initApp() {
+		try {
+			FestivAndesDistributed manager = FestivAndesDistributed.getInstance(new FestivAndesMaster(getPath()));
+			initDataFromFile(manager);
+			System.out.println("InitApp1");
+		} catch (Exception e) {
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}
+		return Response.status(200).build();
+	}
+	
+	/**
+	 * Método que inicializa los atributos basicos de JMSManager
+	 * 
+	 */
+	public void initDataFromFile(FestivAndesDistributed manager) {
+		try {
+			String contextPathP = context.getRealPath("WEB-INF/ConnectionData");
+			String connectionDataPath = contextPathP + FestivAndesMaster.CONNECTION_DATA_FILE_NAME_REMOTE;
+			
+			File arch = new File(connectionDataPath);
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(arch);
+			prop.load(in);
+			in.close();
+			String myQueue = prop.getProperty("myQueue");
+//			String topicAllRentabilidad = prop.getProperty("topicAllRentabilidad");	
+//			String topicAllRentabilidad2 = prop.getProperty("topicAllRentabilidad2");
+			String topicAllFunciones = prop.getProperty("topicAllFunciones");
+//			String topicAllAbonamientos = prop.getProperty("topicAllAbonamientos");
+//			String topicAllRetirarCompania = prop.getProperty("topicAllRetirarCompania");
+//			
+			int numberApps = Integer.parseInt(prop.getProperty("numberApps"));
+			
+//			manager.setUpJMSManager(numberApps, myQueue, topicAllRentabilidad);	
+//			manager.setUpJMSManager(numberApps, myQueue, topicAllRentabilidad2);
+			manager.setUpJMSManager(numberApps, myQueue, topicAllFunciones);
+//			manager.setUpJMSManager(numberApps, myQueue, topicAllAbonamientos);
+//			manager.setUpJMSManager(numberApps, myQueue, topicAllRetirarCompania);	
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
